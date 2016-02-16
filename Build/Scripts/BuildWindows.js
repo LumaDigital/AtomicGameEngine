01218a94096095e89380cfa685b0206fba4f51da
Matt Benic <mattbenic@gmail.com>
2016-02-16 13:32:53 +0200

    Updated BuildWindows to copy all files from editor build directory to fix missing dll error.

--- Build/Scripts/BuildWindows.js
+++ Build/Scripts/BuildWindows.js
@@ -10,10 +10,8 @@ var editorAppFolder = host.artifactsRoot + "AtomicEditor/";
 function PackageBinariesAndResources() {
 
   // Copy the Editor binaries
-  fs.copySync(buildDir + "Source/AtomicEditor/Release/AtomicEditor.exe",
-    host.artifactsRoot + "AtomicEditor/AtomicEditor.exe");
-  fs.copySync(buildDir + "Source/AtomicEditor/Release/D3DCompiler_47.dll",
-    host.artifactsRoot + "AtomicEditor/D3DCompiler_47.dll");
+  fs.copySync(buildDir + "Source/AtomicEditor/Release",
+    host.artifactsRoot + "AtomicEditor");
 
   // We need some resources to run
   fs.copySync(atomicRoot + "Resources/CoreData",
